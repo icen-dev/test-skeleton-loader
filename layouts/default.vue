5fd19dbaeb7f92c93e3ffd71caf43d9ba8ccb233
icen-dev <koike.dev@icen.jp>
2020-05-01 14:53:12 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app right>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="#30475e" dark>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+      <v-spacer />
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    </v-app-bar>
+
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'LayoutsDemosBaselineFlipped',
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  })
+}
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-btn @click="loading = !loading">test</v-btn>
+      <p>Loading: {{ loading }}</p>
+      <v-card width="100vw">
+        <v-skeleton-loader v-if="!loading" max-width="100%" type="image" />
+        <v-img v-if="loading" :src="require('~/assets/sample.jpg')" />
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Vuetify is a progressive Material Design component framework for
+            Vue.js. It was designed to empower developers to create amazing
+            applications.
+          </p>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  components: {},
+  data: () => ({
+    loading: false
+  })
+}
+</script>
